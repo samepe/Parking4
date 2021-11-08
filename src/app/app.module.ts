a95453941064fefc42ea921fea38ad31ce85a435
Paula Rodriguez <mapaurodriguez@hotmail.com>
2021-11-08 08:31:21 -0500

    Añadí los componentes de login y check-in, además de la barra de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+//Librerias de Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Componentes del aplicativo
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateLoginComponent } from './create-login/create-login.component';
 import { CreateCheckInComponent } from './create-check-in/create-check-in.component';
 import { HeaderComponent } from './header/header.component';
+
+//Componentes de Angular Material
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
